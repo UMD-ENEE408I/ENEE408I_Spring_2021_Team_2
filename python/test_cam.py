c403d4a3cd47c5fb1dba505ae578d24c76ecbbec
Adam Martin <adammartin547@gmail.com>
2021-02-16 13:10:12 -0500

    Initial commit

--- python/test_cam.py
+++ python/test_cam.py
@@ -1,10 +1,24 @@
-import cv2 as cv 
-# import  numpy
+from cv2 import cv2
+import  numpy
 from imutils.video import VideoStream
+import imutils
+import time
 
-# vs = VideoStream(src=0).start()
+time.sleep(2.0)
+vs = VideoStream(0)
+    #.start()
 
-# x=1
-# print("ran")
+while True:
+    frame = vs.read()
+    # frame = frame[1]
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('Frame', gray) 
+
+    key = cv2.waitKey(1) & 0xFF
+    # if the 'q' key is pressed, stop the loop
+    if key == ord("q"):
+        break
 
 print('hello')
