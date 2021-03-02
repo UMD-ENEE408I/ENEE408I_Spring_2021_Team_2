e2dcc5d04b7046208d32be89ade06db3935cea6f
phil <jeich52@verizon.net>
2021-03-02 13:41:05 -0500

    Update to Jeopardy Repo playing game and checking strings

--- python/Jeopardy/jeopardy_JSON.py
+++ python/Jeopardy/jeopardy_JSON.py
@@ -4,17 +4,41 @@
   
   
 # importing the module 
+# next: stringUPPER everything
+# different way to loop
 import json 
 import random
-  
+import string
+
+
 # Opening JSON file 
 with open('JEOPARDY_QUESTIONS1.json') as json_file: 
     data = json.load(json_file) 
-  
-    # for reading nested data [0] represents 
-    # the index value of the list 
-    i = random.randint(0, 215000)
-    print(' Category is " ' + data[i]['category'] + ' "\n')
-    print(data[i]['question'] + '\n')
-    print('What is  ' + data[i]['answer'] + '?\n')
-    print(data[i]['value'] + '\n')
+    total = 0
+    if input() == "next question":
+        i = random.randint(0, 215000)
+        print('\nCategory is " ' + data[i]['category'] + ' "\n')
+        print('For ' + data[i]['value'] + '\n')
+        print(data[i]['question'] + '\n')
+        correct_answer = data[i]['answer']
+        correct_answer = correct_answer.replace("\\", "")
+        correct_answer = correct_answer.replace("the", "")
+
+        if correct_answer in input():
+            question_value = (data[i]['value'])
+            nodollarsign_value = question_value.replace("$", "")
+            new_total = int(nodollarsign_value)
+            total = total + new_total
+            print('\nCorrect!\n')
+        else:
+            question_value = (data[i]['value'])
+            nodollarsign_value = question_value.replace("$", "")
+            new_total = int(nodollarsign_value)
+            total = total - new_total
+            print('\nIncorrect. Correct Answer: What is ' + data[i]['answer'] + '?\n')
+
+    elif input() == "check winnings":
+        print('\n$' + str(total) + '\n')
+    elif input() == "quit":
+        print('Game Over. Thanks for playing!')
+        exit() 
