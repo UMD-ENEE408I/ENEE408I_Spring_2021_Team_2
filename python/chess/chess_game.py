7041fd7421329633f9375bd4e41b4d5b772cb599
Adam <adammartin547@gmail.com>
2021-04-28 19:20:19 -0400

    april tags and speaking moves

--- python/chess/chess_game.py
+++ python/chess/chess_game.py
@@ -3,7 +3,10 @@ import chess
 import chess.engine
 import time
 from chess_vision import ChessCamera
-
+import random
+from gtts import gTTS
+import os
+from playsound import playsound
 
 def player_piece_list(board, color):
     pieces = []
@@ -13,11 +16,40 @@ def player_piece_list(board, color):
     pieces = sorted(sum(pieces, []))
     return pieces
 
+
+def print_board(mask):
+    # for testing
+    # prints out the board that the camera sees
+    i = 0
+    for row in range(8):
+        for col in range(8):
+            if mask[i] == chess.WHITE:
+                print('O', end='')
+            elif mask[i] == chess.BLACK:
+                print('G', end='')
+            else:
+                print(' ', end='')
+            i = i+1
+        print('\n', end='')
+    print()
+
+
+
+
 def get_player_move_camera(board, color, cam):
+    # create a list of spaces occupied by the player on the current board
     board_list = player_piece_list(board, color)
-    while True: # loop until a valid move is found
-        time.sleep(2)
+    first_move_seen = None
+    # loop until a valid move is found
+    while True: 
+        # time.sleep(1) # -- this makes it run too slow currently
+
+        # capture the state of the board (list of T/F/None)
         mask = cam.current_colored_board_mask()
+        # if color == chess.WHITE:
+        mask.reverse()
+
+        # select entries in this list that match the players color
         camera_list = []
         for i in range(64):
             if mask[i] == player_color:
@@ -26,19 +58,22 @@ def get_player_move_camera(board, color, cam):
 
         # The player cannot gain or lose pieces on their turn
         if(len(camera_list) != len(board_list)):
-            print("Something went wrong") # Go back to capture the next board from camera
+            # Go back to capture the next board from camera
+            # print("Something went wrong (incorrect number of pieces detected)")
             continue
 
         if(camera_list == board_list):
-            print("No move was made yet") # Go back to capture the next board from camera
+            # Go back to capture the next board from camera
+            # print("No move was made yet") 
             continue
-
+        
         source_square = list(set(board_list)-set(camera_list))
         dest_square = list(set(camera_list)-set(board_list))
         
         # Only one piece can move in a turn
         if(len(source_square) != len(dest_square) != 1):
-            print("Something went wrong") # Go back to capture the next board from camera
+            # Go back to capture the next board from camera
+            # print("Something went wrong (more than one piece moved)") 
             continue
         
         try:
@@ -46,62 +81,113 @@ def get_player_move_camera(board, color, cam):
         except:
             continue # invalid move, wait and capture again
 
-        return player_move  # at this point --- some move was made
+        if first_move_seen == None:
+            first_move_seen = player_move
+            print('I think the move is', player_move.uci())
+            for _ in range(40):
+                _ = cam.current_colored_board_mask()
+            continue
+        elif first_move_seen.uci() == player_move.uci():
+            print('I confirmed the move is', player_move.uci())
+            first_move_seen = None
+            return player_move
+        
+        first_move_seen = None
+        
+
+
+        # ask player to confirm move before pushing it?
+        # confirmation = get_player_confirmation(player_move)
+        # if confirmation:
+            # return player_move 
+
+
+def get_player_confirmation(player_move):
+    # currently text-based, move to speaker prompt and audio reply?
+    print("Your current move is", player_move)
+    resp = input("Is this correct? (y/n) ")
+    if resp == 'y':
+        return True
+    else:
+        return False
+
 
 def get_player_move(board):
+    # text mode for testing 
     while True:
         print()
-        source_file = int(input("Source file: "))
-        source_rank = int(input("Source rank: "))
-        dest_file = int(input("Dest file: "))
-        dest_rank = int(input("Dest rank: "))
+        move_str = input("what is your move? ")
+        # source_file = int(input("Source file: "))
+        # source_rank = int(input("Source rank: "))
+        # dest_file = int(input("Dest file: "))
+        # dest_rank = int(input("Dest rank: "))
 
-        try:
-            source = chess.square(source_file, source_rank)
-            print(source)
-            dest = chess.square(dest_file, dest_rank)
-            print(dest)
-            move = board.find_move(source, dest)
-        except:
-            print('You messed up')
-            continue
+        move = chess.Move.from_uci(move_str)
+        if move in board.legal_moves:
+            return move
         
-        return move
+        # try:
+        #     source = chess.square(source_file, source_rank)
+        #     print(source)
+        #     dest = chess.square(dest_file, dest_rank)
+        #     print(dest)
+        #     move = board.find_move(source, dest)
+        # except:
+        #     print('You messed up')
+        #     continue
+        
+        # return move
 
-limit = True
-# level = int(input("What skill level would you like to play against?: "))
-# play_as = input("What color would you like to play as? (W/B): " )
-# if play_as == "W":
-#     player_color = chess.WHITE
-# else:
-#     player_color = chess.BLACK
 
-player_color = chess.WHITE
+def speak(move):
+    # temp = move.uci()
+    # print(move.uci()[0] + move.uci()[1])
+    # print(temp[2:4])
+    
+    move_string = 'My move is ' + move.uci()[0:2] + ' to ' + move.uci()[2:4]
+    myobj = gTTS(text=move_string, lang='en', slow=False)
+    myobj.save('audio.mp3')
+    playsound('audio.mp3')
+    os.remove('audio.mp3')
 
-# stockfish = Stockfish("C:/Users/adamm/Downloads/Senior/408/robo/python/stockfish_13_win_x64", parameters={"UCI_LimitStrength": limit, "UCI_Elo": elo})
+
+level = int(input("What skill level would you like to play against?: "))
+play_as = input("What color would you like to play as? (W/B): " )
+if play_as == "W":
+    player_color = chess.WHITE
+else:
+    player_color = chess.BLACK
+
 engine = chess.engine.SimpleEngine.popen_uci("Stockfish/src/stockfish")
-# engine.configure({"UCI_LimitStrength": True, "UCI_Elo": elo})
-engine.configure({"Skill Level": 1})
+engine.configure({"Skill Level": level})
 
 board = chess.Board()
+camera = ChessCamera()
 print()
 print(board)
-camera = ChessCamera()
 
 while not board.is_game_over():
     if board.turn == player_color:
-        print('hello')
         move = get_player_move_camera(board, player_color, camera)
-        print('player')
+        # move = get_player_move(board)
+        print('\nplayer: ', end='')
     else:
-        result = engine.play(board, chess.engine.Limit(time=0.1))
-        move = result.move
-        print('comp')
+        if (level == 1):
+            i = random.randint(0,len(list(board.legal_moves))-1)
+            move = list(board.legal_moves)[i]
+        else:
+            result = engine.play(board, chess.engine.Limit(time=0.1))
+            move = result.move
+        print('computer: ', end='')
+        speak(move)
 
     board.push(move)
-    print()
     print(move)
     print(board)
+    print()
+
+final = board.outcome()
+print(final.outcome)
 
 
 x=1
--- python/chess/chess_vision.py
+++ python/chess/chess_vision.py
@@ -1,10 +1,12 @@
 import cv2
 import chess
 import numpy as np
-# from chess_contour import ColorFilter
+import imutils
+import time
+import apriltag
 
-# GREEN = WHITE
-# ORANGE = BLACK
+# GREEN = BLACK
+# ORANGE = WHITE
 
 SQUARE_SIZE = 60
 BOARD_SIZE = SQUARE_SIZE * 8
@@ -12,8 +14,8 @@ BOARD_SIZE = SQUARE_SIZE * 8
 def gstreamer_pipeline(
     capture_width=3280,
     capture_height=2464,
-    display_width=1640,
-    display_height=1232,
+    display_width=820,
+    display_height=616,
     framerate=20,
     flip_method=0,
 ):
@@ -36,104 +38,129 @@ def gstreamer_pipeline(
         )
     )
 
+options = apriltag.DetectorOptions(families="tag36h11")
+detector = apriltag.Detector(options)
+
 class ChessCamera(object):
     def __init__(self):
         self._capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER)
-        # self._capture = cv2.VideoCapture(0)
         self._color_detector = SquareColorDetector()
 
     def current_chessboard_frame(self):
         _, frame = self._capture.read()
-        
-        # frame = frame[1]
+        # cam = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER)
+        # _, frame = cam.read()
         h, w = frame.shape[:2]
 
-        # print(frame)
-        # while True:
-        #     _, frame = self._capture.read()
-        #     cv2.imshow("test",frame)
-        #     cv2.waitKey()
         M = get_chessboard_perspective_transform(frame)
-        bgr_img = cv2.warpPerspective(frame, M, (BOARD_SIZE,BOARD_SIZE))
-        img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
-        cv2.imshow("test2",img)
-        return ChessboardFrame(img)
+        bgr_img = cv2.warpPerspective(frame, M, (BOARD_SIZE+2*SQUARE_SIZE,BOARD_SIZE))
+        # cv2.imshow('hi', bgr_img)
+        # cv2.waitKey()
+        # cam.release()
+        return ChessboardFrame(bgr_img)
 
     def current_colored_board_mask(self):
         cbf = self.current_chessboard_frame()
         cbm = [None] * 64
+
+        # loop through each square and detect the color of the piece
         for i in range(64):
             sq = cbf.square_at(i)
             cbm[i] = self._color_detector.detect(sq)
         return cbm
 
-def get_chessboard_perspective_transform(frame):
-    try:
-        M = np.load('chessboard_perspective_transform.npy')
-        return M
-    except:
-        board_size = (7,7)
-        # _, frame = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER).read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # while True:
-        #     # _, frame = self._capture.read()
-        #     cv2.imshow("test",gray)
-        #     cv2.waitKey()
-        found, board_corners = cv2.findChessboardCorners(gray, board_size)
-        print('poop')
-        image_corners = np.float32([[0,0], [BOARD_SIZE,0], [0,BOARD_SIZE], [BOARD_SIZE,BOARD_SIZE]])
-        assert found, "Couldn't find chessboard."
-
-        M = cv2.getPerspectiveTransform(board_corners, image_corners)
-        np.save('chessboard_perspective_transform.npy', M)
-        return M
-
 
 class SquareColorDetector(object):
     def detect(self, square):
-        green_lower = (22, 115, 64)
-        green_upper = (76, 216, 187)
-        orange_lower = (0, 139, 97) # FILL THESE OUT WHEN THE PIECES ARRIVE
-        orange_upper = (39,255,239)
-        green_radius = ColorFilter(square, green_lower, green_upper)
-        orange_radius = ColorFilter(square, orange_lower, orange_upper)
+        # these may need adjusting in different lightings
+        green_lower = (47, 74, 0)
+        green_upper = (81, 247, 255)
+        orange_lower = (0, 74, 0) 
+        orange_upper = (15,247,255)
+        green_radius = color_filter(square, green_lower, green_upper)
+        orange_radius = color_filter(square, orange_lower, orange_upper)
 
+        # decide the occupancy of the square based on the size of the contours found
         if (int(green_radius) < 10 and int(orange_radius) < 10):
             return None
         elif (int(green_radius) > int(orange_radius)):
-            return chess.WHITE
-        else:
             return chess.BLACK
+        else:
+            return chess.WHITE
+    
 
 class ChessboardFrame():
     def __init__(self, img):
         self.img = img
 
     def square_at(self, i):
-        y = BOARD_SIZE - ((i / 8) % 8) * SQUARE_SIZE - SQUARE_SIZE
-        x = (i % 8) * SQUARE_SIZE
+        y = BOARD_SIZE - (int(i / 8) % 8) * SQUARE_SIZE - SQUARE_SIZE
+        x = ((i % 8) * SQUARE_SIZE) + SQUARE_SIZE
         return Square(i, self.img[y:y+SQUARE_SIZE, x:x+SQUARE_SIZE, :])
+
 
 class Square(object):
     def __init__(self, position, raw_img):
         self.position = position
         self.raw_img = raw_img
 
-def colorFilter(self, frame, lower, upper):
-    # def largest_contour(self, frame, lower, upper):
-    # blur it and convert it to the HSV color space
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+
+def get_chessboard_perspective_transform(frame):
+    try:
+        M = np.load('chessboard_perspective_transform.npy')
+        return M
+    except:
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        image_corners = np.float32([[0,0], [BOARD_SIZE+2*SQUARE_SIZE,0], [0,BOARD_SIZE], [BOARD_SIZE+2*SQUARE_SIZE,BOARD_SIZE]])
+
+        # cv2.imshow('img', gray)
+        # cv2.waitKey()
+
+        # brighter_img = cv2.convertScaleAbs(gray, alpha=1.95, beta=-20)
+
+        # cv2.imshow('brighter', brighter_img)
+        # cv2.waitKey()
+
+        april_results = detector.detect(gray)
+        april_centers = []
+        for tag in april_results:
+            april_centers.append([tag.center[0], tag.center[1]])
+            # print(tag.tag_id)
+        # the found corners are one square in from the edge diagonally
+        # calculate the approximate positions of them from other corners
+        # p1 = board_corners[0][0]
+        # p2 = board_corners[8][0]
+        # delX = abs(p1[0] - p2[0])
+        # delY = abs(p1[1] - p2[1])
+
+        # edge_top_left = [board_corners[0][0][0] - delX, board_corners[0][0][1] - delY]
+        # edge_top_right = [board_corners[6][0][0] + delX, board_corners[6][0][1] - delY]
+        # edge_bot_left = [board_corners[42][0][0] - delX, board_corners[42][0][1] + delY]
+        # edge_bot_right = [board_corners[48][0][0] + delX, board_corners[48][0][1] + delY]
+
+        edge_corners = np.array(april_centers, np.float32)
+        # print(edge_corners)
+        # print(image_corners)
+        M = cv2.getPerspectiveTransform(edge_corners, image_corners)
+        np.save('chessboard_perspective_transform.npy', M)
+        return M
+
+
+def color_filter(frame, lower, upper):
+    # string = 'square ' + str(frame.position)
+    # cv2.imshow(string, frame.raw_img)
+    # cv2.waitKey()
+
+    # blur/convert to HSV
+    blurred = cv2.GaussianBlur(frame.raw_img, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-    # construct a mask for the color, then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
     mask = cv2.inRange(hsv, lower, upper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
 
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
+    # find contours in the mask
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
